a8590de878f94cc8095d43dbff59aca4b5385c62
Abdulsamet Şahin <abdulsametsah@gmail.com>
2020-11-17 18:32:10 +0300

    hello world

---
+++ hello-world/src/main/java/dev/asametsahin/springlearning/helloworld/BaseController.java
@@ -0,0 +1,14 @@
+package dev.asametsahin.springlearning.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class BaseController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
